e18fd3513cda05ff8d8de50b2c57236733b11cb2
Videshway <info@videshway.com>
2026-05-24 03:21:43 +0530

    Update appState.jsx

--- bizzlo-v2/src/lib/appState.jsx
+++ bizzlo-v2/src/lib/appState.jsx
@@ -1064,7 +1064,7 @@ export function AppStateProvider({ children }) {
         const { data: authdata } = await supabase.auth.getSession();
         const token = authdata && authdata.session && authdata.session.access_token;
     const { data, error } = await supabase.functions.invoke('invite-user', {
-      headers: { authorization: 'Bearer ' + token },
+      headers: { Authorization: 'Bearer ' + token },
       body: { account_request_id: requestId, password },
     });
 
